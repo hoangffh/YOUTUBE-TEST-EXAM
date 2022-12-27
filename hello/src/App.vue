19a44d8b6369ee47eacf39ad64d50214b18863f8
hoangffh <nhathoangx06@gmail.com>
2022-12-27 20:38:56 +0700

    test

--- hello/src/App.vue
+++ hello/src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="app">
-   
+   <TheHeader></TheHeader>
+   <TheSideBar></TheSideBar>
   </div>
 </template>
 
 <script>
-
-
+import TheHeader from '../src/components/layout/TheHeader.vue'
+import TheSideBar from "../src/components/layout/TheSideBar.vue"
+// import TheContainer from "../src/components/layout/TheContainer.vue"
 export default {
   name: 'App',
   components: {
-
+    TheHeader,
+    TheSideBar,
+    // TheContainer
   }
 }
 </script>
 
 <style>
-
+@import url(./css/main.css);
 </style>
